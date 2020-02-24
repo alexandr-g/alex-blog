81b5730c565e444d760e7346fc70a84c5a645f21
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-02-24 10:10:07 +0100

    add materialize

--- src/html.jsx
+++ src/html.jsx
@@ -1,30 +1,30 @@
 /* eslint import/no-unresolved:"off" */
 /* eslint import/extensions:"off" */
 /* eslint global-require:"off" */
-import React from "react";
-import favicon from "./favicon.png";
+import React from 'react'
+import favicon from './favicon.png'
 
-let inlinedStyles = "";
-if (process.env.NODE_ENV === "production") {
+let inlinedStyles = ''
+if (process.env.NODE_ENV === 'production') {
   try {
     /* eslint import/no-webpack-loader-syntax: off */
-    inlinedStyles = require("!raw-loader!../public/styles.css");
+    inlinedStyles = require('!raw-loader!../public/styles.css')
   } catch (e) {
     /* eslint no-console: "off" */
-    console.log(e);
+    console.log(e)
   }
 }
 
 export default class HTML extends React.Component {
   render() {
-    let css;
-    if (process.env.NODE_ENV === "production") {
+    let css
+    if (process.env.NODE_ENV === 'production') {
       css = (
         <style
           id="gatsby-inlined-css"
           dangerouslySetInnerHTML={{ __html: inlinedStyles }}
         />
-      );
+      )
     }
     return (
       <html lang="en">
@@ -49,6 +49,11 @@ export default class HTML extends React.Component {
             href="//fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400"
           />
 
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css"
+          />
+
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
           {css}
@@ -61,6 +66,6 @@ export default class HTML extends React.Component {
           {this.props.postBodyComponents}
         </body>
       </html>
-    );
+    )
   }
 }
