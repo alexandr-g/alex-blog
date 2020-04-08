668a7ac95577e78745ac6c1a62c7528706de735b
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-08 17:47:40 +0200

    update site config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -8,8 +8,7 @@ module.exports = {
     'https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png', // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: 'http://grischuk.de', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
-  siteDescription:
-    'Professional Software Engineer living and working in Berlin', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Software Engineer living and working in Berlin since 2015', // Website description used for RSS feeds/meta description tag.
   siteCover:
     'https://images.unsplash.com/photo-1510936111840-65e151ad71bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bcd2bf5b6271090e70ea12980ddb02d7&auto=format&fit=crop&w=2567&q=80', // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: false, // If navigation is enabled the Menu button will be visible
@@ -20,9 +19,9 @@ module.exports = {
   googleAnalyticsID: 'UA-123032104-1', // GA tracking ID.
   siteSocialUrls: [
     'https://github.com/alexandr-g',
+    'https://de.linkedin.com/in/alxgi',
     'mailto:alexgrischuk@gmail.com',
-    'https://twitter.com/alxgri',
-    'https://www.instagram.com/centra_l/'
+    'https://www.instagram.com/centra_l/',
   ],
   postDefaultCategoryID: 'Tech', // Default category for posts.
   // Links to social profiles/projects you want to display in the navigation bar.
@@ -30,31 +29,31 @@ module.exports = {
     {
       label: 'GitHub',
       url: 'https://github.com/alexandr-g',
-      iconClassName: 'fa fa-github' // Disabled, see Navigation.jsx
+      iconClassName: 'fa fa-github', // Disabled, see Navigation.jsx
     },
     {
-      label: 'Twitter',
-      url: 'https://twitter.com/alxgri',
-      iconClassName: 'fa fa-twitter' // Disabled, see Navigation.jsx
+      label: 'LinkedIn',
+      url: 'https://de.linkedin.com/in/alxgi',
+      iconClassName: 'fa fa-linkedin', // Disabled, see Navigation.jsx
     },
     {
       label: 'Email',
       url: 'mailto:alexgrischuk@gmail.com',
-      iconClassName: 'fa fa-envelope' // Disabled, see Navigation.jsx
+      iconClassName: 'fa fa-envelope', // Disabled, see Navigation.jsx
     },
     {
       label: 'Instagram',
       url: 'https://www.instagram.com/centra_l/',
-      iconClassName: 'fa fa-instagram'
-    }
+      iconClassName: 'fa fa-instagram',
+    },
   ],
   // Copyright string for the footer of the website and RSS feed.
   copyright: {
-    label: 'Alexander Grischuk' // Label used before the year
-    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    label: 'Alexander Grischuk', // Label used before the year
+    year: '2018 - 2020', // optional, set specific copyright year or range of years, defaults to current year
     // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
   },
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: '#e0e0e0', // Used for setting manifest background color.
-  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+  promoteGatsby: true, // Enables the GatsbyJS promotion information in footer.
 }
