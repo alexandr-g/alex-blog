68ed5dc1b0080a6cc06f0a52cd4417071cf7b67b
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-08 18:38:45 +0200

    remove subscribe rss button component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,53 +1,52 @@
-import React, { Component, createElement, isValidElement } from "react";
-import GetNavList from "./GetNavList";
-import "./Navigation.css";
-import SubscribeButton from "../SubscribeButton/SubscribeButton";
+import React, { Component, createElement, isValidElement } from 'react'
+import GetNavList from './GetNavList'
+import './Navigation.css'
 
-const Divider = () => null;
+const Divider = () => null
 
-const Subheader = props => {
-  const { primaryText } = props;
-  return <h3>{primaryText}</h3>;
-};
+const Subheader = (props) => {
+  const { primaryText } = props
+  return <h3>{primaryText}</h3>
+}
 
-const ListItem = props => {
-  const { primaryText, component, ...remainingProps } = props;
+const ListItem = (props) => {
+  const { primaryText, component, ...remainingProps } = props
   return (
     <li className="nav-opened" role="presentation">
       {createElement(component, remainingProps, primaryText)}
     </li>
-  );
-};
+  )
+}
 
 const mapToListParts = (item, index) => {
-  if (typeof item === "string" || typeof item === "number") {
-    return createElement(ListItem, { key: item, primaryText: item });
+  if (typeof item === 'string' || typeof item === 'number') {
+    return createElement(ListItem, { key: item, primaryText: item })
   } else if (isValidElement(item)) {
-    return item;
+    return item
   }
 
-  const { divider, subheader, nestedItems, ...remainingProps } = item;
-  let component;
+  const { divider, subheader, nestedItems, ...remainingProps } = item
+  let component
   if (divider) {
-    component = Divider;
+    component = Divider
   } else if (subheader) {
-    component = Subheader;
+    component = Subheader
   } else {
-    component = ListItem;
+    component = ListItem
   }
 
-  const props = { ...remainingProps, key: item.key || index };
+  const props = { ...remainingProps, key: item.key || index }
   if (nestedItems) {
-    props.nestedItems = nestedItems.map(mapToListParts);
+    props.nestedItems = nestedItems.map(mapToListParts)
   }
 
-  return createElement(component, props);
-};
+  return createElement(component, props)
+}
 
 class Navigation extends Component {
   render() {
-    const { config, onClose } = this.props;
-    const navItems = GetNavList(config);
+    const { config, onClose } = this.props
+    const navItems = GetNavList(config)
     return (
       <div>
         <div className="nav">
@@ -56,12 +55,11 @@ class Navigation extends Component {
             <span className="hidden">Close</span>
           </a>
           <ul>{navItems.map(mapToListParts)}</ul>
-          <SubscribeButton url={config.siteRss} />
         </div>
         <span className="nav-cover" />
       </div>
-    );
+    )
   }
 }
 
-export default Navigation;
+export default Navigation
