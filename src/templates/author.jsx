d649096b440dce019f855ce3faabe2e66feca4cf
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-11 19:22:54 +0200

    more updates

--- src/templates/author.jsx
+++ src/templates/author.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
+import React from 'react'
+import Helmet from 'react-helmet'
 import PostListing from '../components/PostListing/PostListing'
 import config from '../../data/SiteConfig'
 import Drawer from '../components/Drawer/Drawer'
@@ -20,6 +20,7 @@ import AuthorWebsite from '../components/AuthorWebsite/AuthorWebsite'
 import AuthorStats from '../components/AuthorStats/AuthorStats'
 import Footer from '../components/Footer/Footer'
 import SocialMediaIcons from '../components/SocialMediaIcons/SocialMediaIcons'
+import Layout from '../components/layout'
 
 class AuthorTemplate extends React.Component {
   state = {
@@ -62,47 +63,49 @@ class AuthorTemplate extends React.Component {
     const getAuthor = () => authorsEdges[0].node
 
     return (
-      <Drawer className="author-template" isOpen={this.state.menuOpen}>
-        <Helmet title={`Posts by "${author}" | ${config.siteTitle}`} />
+      <Layout location={this.props.location}>
+        <Drawer className="author-template" isOpen={this.state.menuOpen}>
+          <Helmet title={`Posts by "${author}" | ${config.siteTitle}`} />
 
-        {/* The blog navigation links */}
-        <Navigation config={config} onClose={this.handleOnClose} />
+          {/* The blog navigation links */}
+          <Navigation config={config} onClose={this.handleOnClose} />
 
-        <SiteWrapper>
-          <MainHeader className="author-head" cover={cover}>
-            <MainNav>
-              <BlogLogo logo={config.siteLogo} title={config.siteTitle} />
-              <MenuButton
-                navigation={config.siteNavigation}
-                onClick={this.handleOnClick}
-              />
-            </MainNav>
-          </MainHeader>
+          <SiteWrapper>
+            <MainHeader className="author-head" cover={cover}>
+              <MainNav>
+                <BlogLogo logo={config.siteLogo} title={config.siteTitle} />
+                <MenuButton
+                  navigation={config.siteNavigation}
+                  onClick={this.handleOnClick}
+                />
+              </MainNav>
+            </MainHeader>
 
-          <AuthorProfile className="inner">
-            <AuthorImage author={getAuthor()} />
-            <AuthorName name={getAuthor().name} />
-            <AuthorBio bio={getAuthor().bio} />
-            <AuthorMeta>
-              <AuthorLocation location={getAuthor().location} />
-              <AuthorWebsite url={getAuthor().url} />
-            </AuthorMeta>
-            <AuthorStats postEdges={postEdges} />
-          </AuthorProfile>
+            <AuthorProfile className="inner">
+              <AuthorImage author={getAuthor()} />
+              <AuthorName name={getAuthor().name} />
+              <AuthorBio bio={getAuthor().bio} />
+              <AuthorMeta>
+                <AuthorLocation location={getAuthor().location} />
+                <AuthorWebsite url={getAuthor().url} />
+              </AuthorMeta>
+              <AuthorStats postEdges={postEdges} />
+            </AuthorProfile>
 
-          {/* PostListing component renders all the posts */}
-          <PostListing postEdges={postEdges} postAuthors={authorsEdges} />
+            {/* PostListing component renders all the posts */}
+            <PostListing postEdges={postEdges} postAuthors={authorsEdges} />
 
-          {/* Social information here */}
-          <SocialMediaIcons urls={getAuthor().socialUrls} />
+            {/* Social information here */}
+            <SocialMediaIcons urls={getAuthor().socialUrls} />
 
-          {/* The tiny footer at the very bottom */}
-          <Footer
-            copyright={config.copyright}
-            promoteGatsby={config.promoteGatsby}
-          />
-        </SiteWrapper>
-      </Drawer>
+            {/* The tiny footer at the very bottom */}
+            <Footer
+              copyright={config.copyright}
+              promoteGatsby={config.promoteGatsby}
+            />
+          </SiteWrapper>
+        </Drawer>
+      </Layout>
     )
   }
 }
@@ -133,10 +136,10 @@ export const pageQuery = graphql`
         }
       }
     }
-    allAuthorsJson(filter: { id: { eq: $author } }) {
+    allAuthorsJson(filter: { uid: { eq: $author } }) {
       edges {
         node {
-          id
+          uid
           name
           image
           url
--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -1,7 +1,9 @@
+import { graphql } from 'gatsby'
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import Helmet from 'react-helmet'
 import PostListing from '../components/PostListing/PostListing'
 import config from '../../data/SiteConfig'
+import Layout from '../components/layout'
 
 class CategoryTemplate extends React.Component {
   render() {
@@ -9,12 +11,14 @@ class CategoryTemplate extends React.Component {
     const postEdges = this.props.data.allMarkdownRemark.edges
     const authorsEdges = this.props.data.authors.edges
     return (
-      <div className="category-container">
-        <Helmet
-          title={`Posts in category "${category}" | ${config.siteTitle}`}
-        />
-        <PostListing postEdges={postEdges} postAuthors={authorsEdges} />
-      </div>
+      <Layout location={this.props.location}>
+        <div className="category-container">
+          <Helmet
+            title={`Posts in category "${category}" | ${config.siteTitle}`}
+          />
+          <PostListing postEdges={postEdges} postAuthors={authorsEdges} />
+        </div>
+      </Layout>
     )
   }
 }
@@ -48,7 +52,7 @@ export const pageQuery = graphql`
     authors: allAuthorsJson {
       edges {
         node {
-          id
+          uid
           name
           image
           url
