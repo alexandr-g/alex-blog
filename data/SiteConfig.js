4399d49b3b507d06057e28477a8d23edef1ae6f5
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-20 14:07:57 +0200

    update copyright

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -49,7 +49,7 @@ module.exports = {
   ],
   // Copyright string for the footer of the website and RSS feed.
   copyright: {
-    label: 'Alexander Grischuk', // Label used before the year
+    label: 'Alexander Grischuk - Frontend Engineer - React', // Label used before the year
     year: '2018 - 2020', // optional, set specific copyright year or range of years, defaults to current year
     // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
   },
