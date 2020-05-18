b8e72221b90b281e551f649eefe48122150c01eb
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-05-18 16:18:35 +0200

    refactor AuthorInfo component

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -6,39 +6,40 @@ import AuthorLocation from '../AuthorLocation/AuthorLocation'
 import AuthorWebsite from '../AuthorWebsite/AuthorWebsite'
 import AuthorLink from '../AuthorLink/AuthorLink'
 
-const Bio = props => {
+const Bio = (props) => {
   const { bio, morePostsUrl } = props
   if (bio) {
     return <p>{bio}</p>
   }
   return (
     <p>
-      Read <Link to={morePostsUrl}>more posts</Link> by this author.
+      Read
+      <Link to={morePostsUrl}>more posts</Link>
+      by this author.
     </p>
   )
 }
 
-class AuthorInfo extends React.Component {
-  render() {
-    const { prefix } = this.props
-    const { id, name, image, bio, url, location } = this.props.author
-    const authorInfoUrl = prefix ? `${prefix}/${id}` : id
-    if (image) {
-      return (
-        <section className="author">
-          <h4>
-            <AuthorLink url={authorInfoUrl} name={name} />
-          </h4>
-          <Bio bio={bio} morePostsUrl={authorInfoUrl} />
-          <AuthorMeta>
-            <AuthorLocation location={location} />
-            <AuthorWebsite url={url} />
-          </AuthorMeta>
-        </section>
-      )
-    }
-    return null
+function AuthorInfo({ prefix, author }) {
+  const { id, name, image, bio, url, location } = author
+  const authorInfoUrl = prefix ? `${prefix}/${id}` : id
+
+  if (image) {
+    return (
+      <section className="author">
+        <h4>
+          <AuthorLink url={authorInfoUrl} name={name} />
+        </h4>
+        <Bio bio={bio} morePostsUrl={authorInfoUrl} />
+        <AuthorMeta>
+          <AuthorLocation location={location} />
+          <AuthorWebsite url={url} />
+        </AuthorMeta>
+      </section>
+    )
   }
+
+  return null
 }
 
 export default AuthorInfo
