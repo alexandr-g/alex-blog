78b9bdb5458bb2b00b7517c0d31ed0cd72f5972b
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-06-25 15:05:16 +0200

    add back to blog button to the about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './About.css'
+import PaginationLink from '../PaginationLink/PaginationLink'
 
 const About = () => (
-  <div className="about">
-    <p>
-      Hi there! I’m Alex, <strong>a frontend software developer</strong>,{' '}
-      <strong>OSS contributor</strong>, and former QA. Most of my time
-      programming I'm working with{' '}
-      <strong>JavaScript, TypeScript, React, Redux, GraphQL</strong>.
-    </p>
-    <p>
-      I am originally from the beautiful Kyiv, Ukraine 🇺🇦 and been working and
-      living in Berlin ❤️🇩🇪 since 2015.
-    </p>
-  </div>
+  <Fragment>
+    <div className="about">
+      <p>
+        Hi there! I’m Alex, <strong>a frontend software developer</strong>,{' '}
+        <strong>OSS contributor</strong>, and former QA. Most of my time
+        programming I'm working with{' '}
+        <strong>JavaScript, TypeScript, React, Redux, GraphQL</strong>.
+      </p>
+      <p>
+        I am originally from the beautiful Kyiv, Ukraine 🇺🇦 and been working and
+        living in Berlin ❤️🇩🇪 since 2015.
+      </p>
+    </div>
+    <PaginationLink className="back-button" url={'/'} text="Back to blog" />
+  </Fragment>
 )
 
 export default About
